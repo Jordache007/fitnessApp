8b703fa6d7aa54ee8667df41af13b37bc562c004
Duran <duran.govender@standardbank.co.za>
2023-03-15 16:45:18 +0200

    Initial Commit

---
+++ fitnessPal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fitnessPal
+//
+//  Created by Duran Govender on 2023/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
